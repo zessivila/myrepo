a2b3afff11af8fe18e9b36898a4c0b3d1f0e9311
root <root@ip-172-31-16-146.ec2.internal>
2021-09-22 22:06:14 +0000

    first project

---
+++ project-terraform/ec2_block/creds.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+
+
+
+  access_key = "ASIAYZECXKLN4EGMIXQI"
+
+
+
+  secret_key = "c4DzmeUufWPUIqNCxE90isKF6bzr7X4/MINsZ2Ob"
+
+
+
+  token = "FwoGZXIvYXdzEK3//////////wEaDE22T8CN6IWtz7iuayK0AZXDkeu5RjiKVUB9UZ0AoovjcodQLkiSVhf2DhEHd/3fSHhHCOWuBshNXuJlkSg1dO9lrwIJjv$
+
+
+
+  region = "us-east-1"
+
+
+
+}
+
+
